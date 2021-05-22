40541c3c452e066097b36c2633e0ed14d684c794
Simranjeet Singh <smrnjeet222@gmail.com>
2021-05-23 00:13:30 +0530

    latest-products-api

--- FlyBuy/settings.py
+++ FlyBuy/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
-    'djoser'
+    'djoser',
+
+	'product'
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -128,6 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
